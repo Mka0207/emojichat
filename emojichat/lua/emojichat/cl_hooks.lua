c08e3d52567a6103d180d0b49c1b6d495d66709b
Mka0207 <mka0207@fwkzt.com>
2020-03-14 15:16:56 -0400

    Convar option to disable the chatbox.

--- emojichat/lua/emojichat/cl_hooks.lua
+++ emojichat/lua/emojichat/cl_hooks.lua
@@ -1,12 +1,20 @@
+local disable_chatbox = false
+
+CreateClientConVar("fwkzt_disablechatbox", "0", true, false)
+if GetConVar("fwkzt_disablechatbox"):GetInt() == 1 then
+	disable_chatbox = true
+end
+
 hook.Remove("InitPostEntity", "emojichat_init")
 hook.Add("InitPostEntity", "emojichat_init", function()
-    -- TODO: Can this get called too early?
-    --eChat.AddServerMessage("EmojiChat by Badger")
+	if disable_chatbox then return end
+	
     eChat.buildBox()
 end)
 
 hook.Remove("ChatText", "emojichat_joinleave")
 hook.Add("ChatText", "emojichat_joinleave", function( index, name, text, type )
+	if disable_chatbox then return end
     if type != "chat" then
         eChat.AddServerMessage(text)
         return true
@@ -15,6 +23,7 @@ end)
 
 hook.Remove("PlayerBindPress", "emojichat_hijackbind")
 hook.Add("PlayerBindPress", "emojichat_hijackbind", function(ply, bind, pressed)
+	if disable_chatbox then return end
     if string.sub( bind, 1, 11 ) == "messagemode" then
         local chatMode = CHATMODE_GLOBAL
         if bind == "messagemode2" then
@@ -29,6 +38,7 @@ end)
 hook.Remove("HUDShouldDraw", "emojichat_hidedefault")
 hook.Add("HUDShouldDraw", "emojichat_hidedefault", function( name )
     if name == "CHudChat" then
+		if disable_chatbox then return end
         return false
     end
 end)
